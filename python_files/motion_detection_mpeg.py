324720881e38d647f63b55f094709b5df65729ed
Stephen Strong <stephen.r.strong@saic.com>
2021-03-30 17:57:22 -0400

    testing new video

--- python_files/motion_detection_mpeg.py
+++ python_files/motion_detection_mpeg.py
@@ -1,26 +1,22 @@
-import numpy as np
 import cv2
+import numpy as np
 
-# cams_test = 10
-# for i in range(0, cams_test):
-#     cap = cv2.VideoCapture(i)
-#     test, frame = cap.read()
-#     print("i : "+str(i)+" /// result: "+str(test))
+#  https://www.youtube.com/watch?v=MkcUgPhOlP8&t=305s
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('vtest.avi')
 frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
 frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
 
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+
+out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
+
 ret, frame1 = cap.read()
-
+ret, frame2 = cap.read()
 print(frame1.shape)
-
-while (True):
-    # Capture frame-by-frame
-    ret, frame2 = cap.read()
-        
+while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
-    frame1 = frame2;
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
@@ -30,22 +26,21 @@ while (True):
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
 
-        area = cv2.contourArea(contour)
-        if area < 1200 and area > 600:
+        if cv2.contourArea(contour) < 900:
             cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 0, 255), 3)
+    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
-    cv2.imshow('diff',gray)
+    image = cv2.resize(frame1, (1280,720))
+    out.write(image)
+    cv2.imshow("feed", frame1)
+    frame1 = frame2
+    ret, frame2 = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(40) == 27:
         break
 
-# When everything done, release the capture
-cap.release()
 cv2.destroyAllWindows()
+cap.release()
+out.release()
